4c638aa1ce63b19349e0c1236f9e2c31e77320a6
Preyas Savani <127723049+preyassavani@users.noreply.github.com>
2023-03-27 06:55:39 +0000

    3 files changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 122334
+let accountEmail = "preyas@google.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+console.log(accountId);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33ab"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "a"
+
+let booleanIsLoggenIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggenIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
