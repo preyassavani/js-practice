566e9ddfbb789d85d64116235f455442ee01ce06
Preyas Savani <127723049+preyassavani@users.noreply.github.com>
2023-04-03 09:37:11 +0000

    3 files submission

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,43 @@
 let score = "33ab"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 let isLoggedIn = "a"
 
 let booleanIsLoggenIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggenIn);
+//console.log(booleanIsLoggenIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(true);
+
+let gameCounter = 100
+gameCounter = gameCounter**2
+console.log(gameCounter);
+
+let a = 8;
+let b = 9;
+let temp;
+temp=a;
+a=b;
+b=temp;
+
+console.log(a,b);
+
+
+let a1 = 41
+let a2 = "41"
+if (a1===a2) {
+    console.log("Number is even");
+}
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,6 @@
+const score = 10
+// score++;
+console.log(score + 1);
+
+const bigNumber = 3456543576654356754n
+console.log(typeof bigNumber);
