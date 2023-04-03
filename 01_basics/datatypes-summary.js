169fa2a45c9305ee4308950266d580e98abea330
Preyas Savani <127723049+preyassavani@users.noreply.github.com>
2023-04-03 11:33:55 +0000

    datatype file modified

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -4,3 +4,26 @@ console.log(score + 1);
 
 const bigNumber = 3456543576654356754n
 console.log(typeof bigNumber);
+
+
+
+// ------Stack a& Heap-----
+
+let myName = "Preyas"
+let anotherName = myName
+anotherName = "Savani"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "preyas@gmail.com",
+    upi: "preyas@hdfc"
+}
+let userTwo = userOne
+userTwo.email = "savani@gmail.com"
+userTwo.upi = "savani@idfc"
+
+console.log(userOne);
+console.log(userTwo);
