949a5e92c059c9c7a2e04b97785b1ef29adc70ba
Preyas Savani <127723049+preyassavani@users.noreply.github.com>
2023-04-10 12:37:27 +0000

    Number and Maths learned

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 500
+const balance = new Number(1000)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.556
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ------- Maths --------
+
+// console.log(Math.max(1,9,4,5,2));
+// console.log(Math.min(1,9,4,5,2));
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.6));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
