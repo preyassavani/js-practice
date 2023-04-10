e2b9644089c1aee3b412c04f1f79ff981c3e2f4c
Preyas Savani <127723049+preyassavani@users.noreply.github.com>
2023-04-10 10:25:58 +0000

    strings part is done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Preyas"
+const repoCount = 1
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('Road-swipe-com')
+// console.log(gameName[5]);
+// // console.log(gameName.__proto__);
+// // console.log(gameName.length);
+// // console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('w'));
+const newString = gameName.substring(0,4)
+// console.log(newString);
+const anotherString = gameName.slice(-7,-5)
+// console.log(anotherString);
+const newStringOne = "   Preyas   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+const url = "https://preyas.com/preyas%20savani"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('Mukesh'));
+// console.log(gameName.split('-', 2));
+console.log(gameName.endsWith('m'));
+const name1 = "Preyas"
+const score = 90
+console.log(`Hello, my name is ${name1} and my score is ${score}`);
