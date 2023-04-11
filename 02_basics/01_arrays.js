91ed55311bc26695f4b79d939d3575197b0461f2
Preyas Savani <127723049+preyassavani@users.noreply.github.com>
2023-04-11 12:17:27 +0000

    Array first part

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr[3]);
+
+// myArr.push(6,7)
+// console.log(myArr);
+
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(2));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+console.log("A ", myArr);
+
+const my1 = myArr.slice(1, 3)
+console.log(my1);
+console.log("B ",myArr);
+
+const my2 = myArr.splice(1, 3)
+console.log(my2);
+console.log("C ",myArr);
